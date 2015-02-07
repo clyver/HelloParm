196996e8917305407f418bac108f2e8612a402fa
Christopher Lyver <lyverc@ccs.neu.edu>
2015-02-07 17:18:17 -0500

    Fix a bracket and remove pdb

--- fetch_food.py
+++ fetch_food.py
@@ -1,10 +1,8 @@
 from ingredients import food2fork
 from keys import Keys
-import pdb
 
-pdb.set_trace()
 keys = Keys()
-test_call = food2fork(keys.get_food_key()
+test_call = food2fork(keys.get_food_key())
 
 data = test_call.getRecipe(25359)
 

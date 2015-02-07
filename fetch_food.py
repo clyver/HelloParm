c98dcb565274bc33e4ff3b9a787fdf2eab5868b2
lyvert <lyvert@fordhamprep.org>
2015-02-07 14:53:34 -0500

    Fetch ingredients with API

---
+++ fetch_food.py
@@ -0,0 +1,11 @@
+from ingredients import food2fork
+from keys import Keys
+import pdb
+
+pdb.set_trace()
+keys = Keys()
+test_call = food2fork(keys.get_food_key()
+
+data = test_call.getRecipe(25359)
+
+print data 

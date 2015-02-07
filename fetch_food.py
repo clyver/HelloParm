4ed324028ca45d4ad02dea9fdf57ce7add3fecfe
Christopher Lyver <lyverc@ccs.neu.edu>
2015-02-07 18:42:50 -0500

    Ingredients are constant, don't make an API call for them

--- fetch_food.py
+++ fetch_food.py
@@ -1,13 +1,9 @@
 from ingredients import food2fork
 from keys import Keys
 
-# Call our secrets
-keys = Keys()
+parm_perfection = {"cup plain dry bread crumbs": .75, "tsp. Italian seasoning": .5,  "tsp. garlic powder": .25, "boneless, skinless chicken breast halves (about 2 lbs)": 6, "egg, beaten": 1, "jar Rao's Traditional Sauce": 1, "cup shredded mozzarella cheese": 1}
 
-# Instantiate the food2fork api
-ingredients_api = food2fork(keys.get_food_key())
-
-# Get the ingredients for chickenparm with its id
-parm_ingredients = ingredients_api.getRecipe(25359)
-
+def serving_size(servings):
+	for k, v in parm_perfection.iteritems():
+		parm_perfection[k]= v*servings
 

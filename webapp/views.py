83b7f9969b5aff48abfe67e63de79c9898298a4f
Bobby Singh <bobby.singh@urbancompass.com>
2015-02-07 19:15:06 -0500

    Added form validation response.

--- webapp/views.py
+++ webapp/views.py
@@ -5,5 +5,7 @@ from webapp.forms import ChickenParmForm
 @app.route('/', methods=["POST"])
 def parm_me_bro():
   form = ChickenParmForm()
+  if form.validate_on_submit():
+    flash('You can eat %d parms?!"', form.num_parms.data) 
   return render_template('index.html', title='An Ode to Parmesan', form=form)
 

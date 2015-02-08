612db5a6c1f2a9362f1085db8428b8a02e2ee695
Christopher Lyver <lyverc@ccs.neu.edu>
2015-02-07 19:13:52 -0500

    Get route method right

--- webapp/views.py
+++ webapp/views.py
@@ -2,7 +2,8 @@ from flask import render_template
 from webapp import app
 from webapp.forms import ChickenParmForm
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def parm_me_bro():
   form = ChickenParmForm()
   return render_template('index.html', title='An Ode to Parmesan', form=form)
+
